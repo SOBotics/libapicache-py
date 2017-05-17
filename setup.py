e9a0f3a77272eeb4f4e9a989eb03168607ffecd4
ArtOfCode- <hello@artofcode.co.uk>
2017-05-17 15:00:10 +0100

    0.0.6

--- setup.py
+++ setup.py
@@ -4,12 +4,12 @@ from distutils.core import setup
 setup(
     name='libapicache',
     packages=['libapicache'],
-    version='0.0.5',
+    version='0.0.6',
     description='Python interface library for APICache (https://github.com/SOBotics/apicache)',
     author='ArtOfCode',
     author_email='art@artofcode.co.uk',
     url='https://github.com/SOBotics/libapicache-py',
-    download_url='https://github.com/SOBotics/libapicache-py/archive/0.0.5.tar.gz',
+    download_url='https://github.com/SOBotics/libapicache-py/archive/0.0.6.tar.gz',
     keywords=['library', 'caching'],
     classifiers=[],
     requires=['requests']
